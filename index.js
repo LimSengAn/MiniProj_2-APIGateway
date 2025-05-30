2d36835dd69e4dfe9626217d0c8c6dfd25cd54b5
LimSengAn <2023453lim@aupp.edu.kh>
2025-05-30 16:17:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const proxy = httpProxy.createProxyServer({});
 
 app.use('/student', (req, res) => {
     console.log('INSIDE API GATEWAY STUDENT ROUTE');
-    proxy.web(req, res, { target: '100.27.18.171' });
+    proxy.web(req, res, { target: 'http://100.27.18.171:5000' });
 })
 
 app.use('/teacher', (req, res) => {
     console.log('INSIDE API GATEWAY TEACHER ROUTE');
-    proxy.web(req, res, { target: '44.203.41.101' });
+    proxy.web(req, res, { target: 'http://44.203.41.101:5001' });
 })
 
 app.listen(4000, () => {
